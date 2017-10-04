24d4d685a67b5d3e0773f69ce545b5867159df8c
drklrd <srvbhattarai@gmail.com>
2017-10-04 08:11:06 +0545

    get location

--- app/src/main/java/com/example/drklrd/wheremi/MainActivity.java
+++ app/src/main/java/com/example/drklrd/wheremi/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.drklrd.wheremi;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void updateLocation(Location location){
+        Log.i("Changed***",location.toString());
+    }
+
+    public void startListening(){
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Log.i("PHEI","KANCHGGA***");
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            startListening();
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("$$$$$$$$$$$$$","**************");
+                updateLocation(location);
+                TextView altitudeText = (TextView) findViewById(R.id.altitudeText);
+                TextView latitudeText = (TextView) findViewById(R.id.latitudeText);
+                TextView longitudeText = (TextView) findViewById(R.id.longitudeText);
+
+                altitudeText.setText(location.getAltitude() + "m");
+                latitudeText.setText(String.valueOf(location.getLatitude()));
+                longitudeText.setText(String.valueOf(location.getLongitude()));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if(Build.VERSION.SDK_INT < 23) {
+            Log.i("HELLO***","NICE222");
+
+            startListening();
+        }else{
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }else{
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                if(location != null){
+                    updateLocation(location);
+                }else{
+                    Log.i("BABBA","BBBA**");
+                    startListening();
+                }
+
+            }
+        }
     }
 }
